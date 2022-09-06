cb494eef972db77d8888cd01e0f522a4fcae3845
NikolayD <kolik885@ya.ru>
2022-09-06 22:30:11 +0700

    Не могу вызвать функцию CreateArrayWithRandomNumbers

---
+++ 058/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+
+Console.Clear();
+
+void PrintArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateArrayWithRandomNumbers(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    var random = new Random();
+
+    for (var i = 0; i < result.GetLength(0); i++)
+    {
+        for (var j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = random.Next(1, 10);
+        }
+    }
+    return result;
+}
+
+Console.WriteLine("Введите размерность матрицы (m)");
+if (!int.TryParse(Console.ReadLine()!, out var m))
+{
+    Console.WriteLine("Ошибка. Введите число");
+}
+Console.WriteLine("Введите число столбцов (n)");
+if (!int.TryParse(Console.ReadLine()!, out var n))
+{
+    Console.WriteLine("Ошибка. Введите число");
+}
+//var n = m;
+var array = CreateArrayWithRandomNumbers(m, n);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+
+void Aa(int[,] array)
+{
+
+    int[,] matrixA = new int[array.GetLength(0), array.GetLength(1)];
+    CreateArrayWithRandomNumbers(matrixA);
+
+    int[,] matrixB = new int[array.GetLength(0), array.GetLength(1)];
+    CreateArrayWithRandomNumbers(matrixB);
+
+    int[,] matrixC = new int[array.GetLength(0), array.GetLength(1)];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+            }
+        }
+    }
+}
+Console.WriteLine("Матрица А");
+PrintArray(matrixA);
+Console.WriteLine();
+Console.WriteLine("Матрица В");
+PrintArray(matrixB);
+Console.WriteLine();
+Console.WriteLine("Произведение матриц А*В");
+PrintArray(matrixC);
